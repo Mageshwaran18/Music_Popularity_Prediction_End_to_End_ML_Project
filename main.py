f52e8a684da4e61dab9b337dd01bb330abf22189
Mageshwaran <mageshwaranp18@gmail.com>
2024-06-04 21:25:49 +0530

    Logging is created in the src.project's Constructor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.Music_Popularity_Prediction_End_to_End_ML_Project.config import configuration
-from src.Music_Popularity_Prediction_End_to_End_ML_Project.logging import logger
+from src.Music_Popularity_Prediction_End_to_End_ML_Project import logger
 
 logger.info("This is our custom log")
