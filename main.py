773bcf95eabe5aac7e6c8007d61af9a0038043b0
Mageshwaran <mageshwaranp18@gmail.com>
2024-06-04 19:54:16 +0530

    Custom_log_is_created

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from src.Music_Popularity_Prediction_End_to_End_ML_Project.config import configuration
+from src.Music_Popularity_Prediction_End_to_End_ML_Project.logging import logger
+
+logger.info("This is our custom log")
