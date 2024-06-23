8f1944eed6734fc1e1e795610027c898059b837c
Mageshwaran <mageshwaranp18@gmail.com>
2024-06-23 16:35:47 +0530

    Data_Ingestion using modulor coding is done

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from src.Music_Popularity_Prediction_End_to_End_ML_Project.config import configuration
 from src.Music_Popularity_Prediction_End_to_End_ML_Project import logger
+from src.Music_Popularity_Prediction_End_to_End_ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is our custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/Music_Popularity_Prediction_End_to_End_ML_Project/entity/config_entity.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+# Both the variable in the DataIngestion of config.yaml and the variable of DataIngestionConfig should match
